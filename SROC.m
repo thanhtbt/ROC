43a8a6505427d7ff9ce3063284b1dfc3dbd558ae
LE Trung-Thanh <letrungthanhtbt@gmail.com>
2021-02-08 10:30:24 +0100

    Update SROC.m

--- SROC.m
+++ SROC.m
@@ -4,36 +4,41 @@ load SEN
 load SPE
 
 %% Probit
-close all;
 SEN_log = [];
 a = 3.8;
 b = 0.24;
 SEN_pro = [];
 SEN_pro_AUC = [];
+
 x = linspace(0,1,121);
 for ii = 1:length(SEN)
-    r = 1/2 + 1/2*erf( (1+b)/(1-b)*erfinv(1-2*SPE(ii)) + a*sqrt(pi)/(4 -4*b) );
+
+    r = 1/2 + 1/2*erf( (1+b)/(1-b)*erfinv(1-2*SPE(ii)) + a*sqrt(pi)/(4-4*b) );
     SEN_pro = [SEN_pro; r];
-    r = 1/2 + 1/2*erf( (1+b)/(1-b)*erfinv(1-2*x(ii)) + a*sqrt(pi)/(4 -4*b) );
+    
+    r = 1/2 + 1/2*erf( (1+b)/(1-b)*erfinv(1-2*x(ii)) + a*sqrt(pi)/(4-4*b) );
     SEN_pro_AUC = [SEN_pro_AUC; r];
 end
 
 
-%% New ROC Logit
+%%  Logit
 a = 3.9;
 b = 0.1;
 SEN_log_AUC = [];
+delta = 0.0001;
 for ii = 1: length(SEN)
-    TS      = exp(a/(1-b)) * ((1-SPE(ii))/SPE(ii))^( (1+b)/(1-b) );
-    MS      = 1 + exp(a/(1-b)) * ((1-SPE(ii))/SPE(ii))^( (1+b)/(1-b) );
+
+    TS      = exp(a/(1-b)) * ( (1-SPE(ii))/SPE(ii) )^( (1+b)/(1-b) );
+    MS      = 1 + exp(a/(1-b)) * ( (1-SPE(ii))/SPE(ii) )^( (1+b)/(1-b) );
     SEN_log = [SEN_log; TS/MS];
-    TS      = exp(a/(1-b)) * ((1-x(ii))/(x(ii)+0.0001))^( (1+b)/(1-b) )
-    MS      = 1 + exp(a/(1-b)) * ((1-x(ii))/(0.0001 + x(ii)))^( (1+b)/(1-b) )
-      
-    SEN_log_AUC = [SEN_log_AUC;TS/MS];
+    
+    TS      = exp(a/(1-b)) * ( (1-x(ii))/(x(ii)+delta) )^( (1+b)/(1-b) );
+    MS      = 1 + exp(a/(1-b)) * ( (1-x(ii))/(x(ii)+delta) )^( (1+b)/(1-b) );    
+    SEN_log_AUC = [SEN_log_AUC; TS/MS];
     
 end
 
+%% ERROR
 error_log = abs(SEN -SEN_log );
 error_log = sum(error_log)/121
 
@@ -46,13 +51,14 @@ for ii = 1:120
     S_log = S_log + 1/2*(SEN_log_AUC(ii) + SEN_log_AUC(ii+1))*(x(ii+1)-x(ii));
 end
 S_log
+
 S_pro = 0;
 for ii = 1:120
     S_pro = S_pro + 1/2*(SEN_pro_AUC(ii) + SEN_pro_AUC(ii+1))*(x(ii+1)-x(ii));
 end
 S_pro
 
-
+%% PLOT
 figure; 
 plot(1 - SPE, SEN,'o');hold on;
 plot(1 - SPE',SEN_pro','b');
