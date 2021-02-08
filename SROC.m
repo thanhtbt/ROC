6b83ffcec3cc660a331178f31c3cc0b472ae8625
LE Trung-Thanh <letrungthanhtbt@gmail.com>
2021-02-08 10:54:23 +0100

    Update SROC.m

--- SROC.m
+++ SROC.m
@@ -8,14 +8,12 @@ load SPE
 SEN_log = [];
 a = 3.8;
 b = 0.24;
-SEN_pro = [];
-SEN_pro_AUC = [];
-
-
 N = length(SEN);
 x = linspace(0,1,N);
-for ii = 1 : N
 
+SEN_pro = [];
+SEN_pro_AUC = [];
+for ii = 1 : N
     r = 1/2 + 1/2*erf( (1+b)/(1-b)*erfinv(1-2*SPE(ii)) + a*sqrt(pi)/(4-4*b) );
     SEN_pro = [SEN_pro; r];
     
@@ -30,7 +28,6 @@ b = 0.1;
 SEN_log_AUC = [];
 delta = 0.0001;
 for ii = 1 : N
-
     TS      = exp(a/(1-b)) * ( (1-SPE(ii))/SPE(ii) )^( (1+b)/(1-b) );
     MS      = 1 + exp(a/(1-b)) * ( (1-SPE(ii))/SPE(ii) )^( (1+b)/(1-b) );
     SEN_log = [SEN_log; TS/MS];
@@ -42,10 +39,10 @@ for ii = 1 : N
 end
 
 %% ERROR
-error_log = abs(SEN -SEN_log );
+error_log = abs(SEN -SEN_log);
 error_log = sum(error_log)/N
 
-error_pro = abs(SEN -SEN_pro );
+error_pro = abs(SEN -SEN_pro);
 error_pro = sum(error_pro)/N
 
 %% AUC
@@ -63,10 +60,10 @@ S_pro
 
 %% PLOT
 figure; 
-plot(1 - SPE, SEN,'o');hold on;
-plot(1 - SPE',SEN_pro','b');
-hold on;
-plot(1 - SPE',SEN_log','r');
+plot(1 - SPE, SEN,'o');
+hold on; plot(1 - SPE',SEN_pro','b');
+hold on; plot(1 - SPE',SEN_log','r');
+
 legend('Cost/Benefit Point','Gaussian-based ROC','Logistic-based ROC');
 xlabel('False Positive Rate');
 ylabel('True Positive Rate');
